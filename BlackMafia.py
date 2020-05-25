6b05413092818fa17362165e7211efc1f36aff70
Nishanegi420 <65805165+Nishanegi420@users.noreply.github.com>
2020-05-26 00:09:03 +0300

    Update BlackMafia.py

--- BlackMafia.py
+++ BlackMafia.py
@@ -104,8 +104,8 @@ jalan("\033[1;93m   ┈┈┈┈┈┈┈┈╲┊┊┊┊╱┈┈┈┈┈┈
 jalan("\033[1;93m   ┈┈┈┈┈┈┈┈▕╲▂▂╱▏┈┈┈┈┈┈┈┈")
 print "\033[1;93m♡─────╱▔▔▔▔┊┊┊┊▔▔▔▔╲───────♡\033[1;96mLogin BlackMafia\033[1;95m♡╰──────────•◈•──────────╯♡"
 
-CorrectUsername = "BlackMafia"
-CorrectPassword = "lovehacker"
+CorrectUsername = "Nishanegi"
+CorrectPassword = "xtylishsam"
 
 loop = 'true'
 while (loop == 'true'):
